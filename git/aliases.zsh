6a4cabe1ed271d3500c87bf72b09306503a2ec0f
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2015-06-06 17:00:38 +0100

    Removing spaces before & after '=' when defining aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,13 +1,13 @@
 # git aliases
 #
 
-alias gi  = 'git init'
-alias gcl = 'git clone'
-alias gs  = 'git status -sb'
-alias ga  = 'git add -a'
-alias gco = 'git commt -m'
-alias gb  = 'git branch'
-alias gpu = 'git push'
-alias gpl = 'git pull'
-alias gf  = 'git fetch'
-alias gl  = 'git log'
+alias gi='git init'
+alias gcl='git clone'
+alias gs='git status -sb'
+alias ga='git add -a'
+alias gco='git commt -m'
+alias gb='git branch'
+alias gpu='git push'
+alias gpl='git pull'
+alias gf='git fetch'
+alias gl='git log'
--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,7 +1,7 @@
 # brew aliases
 
-alias bd = 'brew doctor'
-alias bu = 'brew update'
-alias bi = 'brew install'
-alias bs = 'brew search'
-alias bcs = 'brew cask search'
+alias bd='brew doctor'
+alias bu='brew update'
+alias bi='brew install'
+alias bs='brew search'
+alias bcs='brew cask search'
