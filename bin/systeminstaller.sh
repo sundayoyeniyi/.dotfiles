eb44ea344630a9a7708db20eedc84442c0a1e866
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2016-01-24 21:18:05 +0000

    adding pixate-studio

--- bin/systeminstaller.sh
+++ bin/systeminstaller.sh
@@ -42,6 +42,7 @@ brew cask install visual-studio-code
 brew cask install evernote
 brew cask install skype
 brew cask install teamviewer
+brew cask install pixate-studio
 # brew cask install box-sync
 # brew cask install dropbox
 # brew cask install sublime-text
