b92c458281cbd89a1df7b54ee2a8d617750e924f
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2016-02-01 09:50:59 +0000

    Colourful prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,8 +2,10 @@
 #
 # Setting prompt
 
-export PS1='U: %n T: %T P: %/
-%# > '
+autoload -U colors && colors
+
+export PS1='%B%F{blue}%n%f %F{yellow}% %/ %f
+%F{blue}%#%f%F{yellow}<-->%f%b '
 
 export RPS1='%w'
 #TODO = change right prompt to state of git
