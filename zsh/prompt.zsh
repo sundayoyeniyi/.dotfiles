bc7a41c164d1c95ab113205507883d09a7a4ce7b
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2016-02-01 22:05:41 +0000

    changes to prompt colors

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 #
 # Setting prompt
+# color codes
+# 0=black; 1=red; 2=green; 3=yellow; 4=blue; 5=magenta; 6=cyan; 7=white;
 
 autoload -U colors && colors
 
-export PS1='%B%F{red}%n%f %F{green}% %/ %f
-%F{blue}%#%f%F{green}-->%f%b '
+export PS1='%B%F{2}[%F{1}%n%F{2}] %/
+%F{4}%#%F{2}-->%f%b '
 
 export RPS1='%w'
 #TODO = change right prompt to state of git
