57c3bb2c59f4e355d0425b0f94ca9a7628f300e3
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2016-10-30 18:23:59 +0000

    Replace heroku-toolbelt with heroku

--- bin/systeminstaller.sh
+++ bin/systeminstaller.sh
@@ -48,7 +48,7 @@ brew cask install teamviewer
 brew cask install pixate-studio
 brew cask install libreoffice
 brew cask install sketch
-brew cask install heroku-toolbelt
+brew cask install heroku
 # brew cask install box-sync
 # brew cask install dropbox
 
