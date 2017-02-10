b7b10edff213aa1ec42ae7e9f078bf3704ef838a
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2017-02-10 22:44:22 +0000

    Add android SDK to path variable

--- java/javahome.zsh
+++ java/javahome.zsh
@@ -2,5 +2,10 @@
 #
 # Setting Java_Home
 
+# Create a JAVA_HOME variable, determined dynamically
 export JAVA_HOME=$(/usr/libexec/java_home)
 export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
+# Add that to the global PATH variable
+export PATH=${JAVA_HOME}/bin:$PATH
+# Add the Android SDK to the PATH variable
+export PATH=${PATH}:~/Library/Android/sdk/tools:~/Library/Android/sdk/platform-tools
