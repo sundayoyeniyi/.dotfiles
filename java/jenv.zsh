1fe06c70d077a2ea341c19a67042ba38d64ad972
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2019-09-29 11:12:45 +0100

    Explicitly setting java_home

--- java/jenv.zsh
+++ java/jenv.zsh
@@ -6,3 +6,9 @@ export PATH="$HOME/.jenv/bin:$PATH"
   eval "$(jenv init -)"
 
 jenv enable-plugin export
+
+#EXplicitly setting JAVA_HOME
+export JAVA_HOME=$(/usr/libexec/java_home)
+#export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
+# Add that to the global PATH variable
+export PATH=${JAVA_HOME}/bin:$PATH
