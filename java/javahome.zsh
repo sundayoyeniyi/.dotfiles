b43f4ac3374161a1194517c7ab98c996e82451c9
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2019-09-30 06:25:44 +0100

    Removing jenv for manual setting of JAVA_HOME envvar

--- java/javahome.zsh
+++ java/javahome.zsh
@@ -3,7 +3,7 @@
 # Setting Java_Home
 
 # Create a JAVA_HOME variable, determined dynamically
-#export JAVA_HOME=$(/usr/libexec/java_home)
-#export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
+export JAVA_HOME=$(/usr/libexec/java_home)
+export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
 # Add that to the global PATH variable
-#export PATH=${JAVA_HOME}/bin:$PATH
+export PATH=${JAVA_HOME}/bin:$PATH
--- java/jenv.zsh
+++ java/jenv.zsh
@@ -2,11 +2,11 @@
 #
 # Setting up jenv for managing Java Versions
 
-export PATH="$HOME/.jenv/bin:$PATH"
-  eval "$(jenv init -)"
+#export PATH="$HOME/.jenv/bin:$PATH"
+#  eval "$(jenv init -)"
 
 #Explicitly setting JAVA_HOME
-export JAVA_HOME=$(/usr/libexec/java_home)
+#export JAVA_HOME=$(/usr/libexec/java_home)
 #export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
 # Add that to the global PATH variable
-export PATH=${JAVA_HOME}/bin:$PATH
+#export PATH=${JAVA_HOME}/bin:$PATH
