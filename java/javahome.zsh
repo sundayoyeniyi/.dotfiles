222f2752d898e9b8053505aecd70a575a59a2ca0
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2019-09-30 08:01:25 +0100

    Java 11

--- java/javahome.zsh
+++ java/javahome.zsh
@@ -3,7 +3,8 @@
 # Setting Java_Home
 
 # Create a JAVA_HOME variable, determined dynamically
-export JAVA_HOME=$(/usr/libexec/java_home)
-export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
+#export JAVA_HOME=$(/usr/libexec/java_home)
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
+#export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
 # Add that to the global PATH variable
 export PATH=${JAVA_HOME}/bin:$PATH
--- java/jenv.zsh
+++ java/jenv.zsh
@@ -5,6 +5,8 @@
 #export PATH="$HOME/.jenv/bin:$PATH"
 #  eval "$(jenv init -)"
 
+#jenv enable-plugin export
+
 #Explicitly setting JAVA_HOME
 #export JAVA_HOME=$(/usr/libexec/java_home)
 #export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
